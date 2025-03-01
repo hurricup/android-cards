3b01a3f3a37121dc5e7b913a8c03cf097725d156
Alexandr Evstigneev <hurricup@gmail.com>
2025-03-01 09:28:23 +0300

    Initial commit

---
+++ app/src/main/java/com/hurricup/cards/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.hurricup.cards
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.hurricup.cards.ui.theme.AndroidCardsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidCardsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+        textAlign = TextAlign.Center,
+        fontSize = 30.sp
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidCardsTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/hurricup/cards/WordsActivity.kt
@@ -0,0 +1,10 @@
+package com.hurricup.cards
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class WordsActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
