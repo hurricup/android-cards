b895352ffe2bb465bd7f05cb63a188c0ab5619da
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2026-03-11 10:08:06 +0400

    Added zero divisions

--- app/src/main/java/com/hurricup/cards/model/impl/Division.kt
+++ app/src/main/java/com/hurricup/cards/model/impl/Division.kt
@@ -6,7 +6,7 @@ import com.hurricup.cards.model.Questionary
 class Division : Questionary("Деление") {
     override val _questions: MutableList<Question> by lazy {
         val result = mutableListOf<Question>()
-        for (i in 1..10) {
+        for (i in 0..10) {
             for (j in 1..10) {
                 result += Question("${i * j} / $j = ?")
             }
